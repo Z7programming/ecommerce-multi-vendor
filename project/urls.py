c4525b623e1f0c2ecbf787ac4d7be042ca05270c
z7prog <z7programming@gmail.com>
2022-09-11 16:50:31 +0100

    Add static and templates and media

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('Home.urls')),
     path('dachboard/',include('vendor_dach.urls')),
     path('hpanel/',include('admin_dach.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
